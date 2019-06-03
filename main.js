a6a5ede5e7c5583c869ccc4d2b8949445470edba
mafubello <50101167+mafubello@users.noreply.github.com>
2019-06-03 15:52:54 +0100

    Modify landing page

--- main.js
+++ main.js
@@ -1,28 +1,27 @@
-function change1() {
-    document.getElementById('img1').src = '/image/img3.jpg';
-   }
+// function change1() {
+//     document.getElementById('img1').src = './image/img3.jpg';
+//    }
    
-   function change2() {
-       document.getElementById('img1').src = '/image/img1.jpg';
-   }
+//    function change1b() {
+//        document.getElementById('img1').src = '/image/img1.jpg';
+//    }
    
-   function changeimg2() {
-       document.getElementById('img2').src = '/image/img3.jpg';
-   }
+//    function changeimg2() {
+//        document.getElementById('img2').src = '/image/img3.jpg';
+//    }
    
-   function changeimg2b() {
-       document.getElementById('img2').src = '/image/img4.jpg';
-   }
+//    function changeimg2b() {
+//        document.getElementById('img2').src = '/image/img4.jpg';
+//    }
    
-   function changeimg3() {
-       document.getElementById('img4').src = '/image/img4.jpg';
-   }
+//    function changeimg3() {
+//        document.getElementById('img4').src = '/image/img4.jpg';
+//    }
    
-   function changeimg3b() {
-       document.getElementById('img4').src = '/image/img6.jpg';
-   }
-   
-   function submitForm() {
-       var name = document.getElementById("name").value;
-       alert("Thank you " + name);
-   }
+//    function changeimg3b() {
+//        document.getElementById('img4').src = '/image/img6.jpg';
+//    }
+function submitForm() {
+    var name = document.getElementById("name").value;
+    alert("Thank you " + name);
+}
